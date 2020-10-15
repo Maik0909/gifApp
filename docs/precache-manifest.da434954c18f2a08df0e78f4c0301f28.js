e2a537339f8766f84797a894441761653c00f99e
Maikol Pedroza <maikolpedroza0@gmail.com>
2020-10-15 14:55:43 -0500

    first commit

---
+++ docs/precache-manifest.da434954c18f2a08df0e78f4c0301f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd96fa3fd38435ebcdd05782268f240",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e17d04367ae90887c87",
+    "url": "/static/css/main.c00f8e21.chunk.css"
+  },
+  {
+    "revision": "ef7fe5dcd6fe2f866af2",
+    "url": "/static/js/2.c555a145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c555a145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e17d04367ae90887c87",
+    "url": "/static/js/main.98d28588.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
